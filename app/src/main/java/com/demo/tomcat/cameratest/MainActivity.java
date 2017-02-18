e10e6888b326ce6de16e1d0fd409be7120a294d1
tomcat <risc604@ms40.url.com.tw>
2017-02-18 17:26:16 +0800

    album file degree issue NOT yet.

--- app/src/main/java/com/demo/tomcat/cameratest/MainActivity.java
+++ app/src/main/java/com/demo/tomcat/cameratest/MainActivity.java
@@ -2,6 +2,7 @@ package com.demo.tomcat.cameratest;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -9,6 +10,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -35,7 +37,6 @@ public class MainActivity extends AppCompatActivity
     private final static int PHOTO = 99;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -49,30 +50,60 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
+        int degree = 0;
         switch (requestCode)
         {
-
             case PHOTO:
             case CAMERA:
                 if (data != null)
                 {
                     Uri uri = data.getData();
-                    ContentResolver cr = this.getContentResolver();
+                    //ContentResolver cr = this.getContentResolver();
 
                     String[] mMediaDATA = {MediaStore.Images.Media.DATA};
-                    Cursor mCursor = managedQuery(uri, mMediaDATA, null, null, null);
-                    int mImageIndex = mCursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                    //Cursor mCursor = managedQuery(uri, mMediaDATA, null, null, null);
+                    Cursor mCursor = this.getContentResolver().query(uri, mMediaDATA, null, null, null);
+                    Log.d(TAG, "uri: " + uri + ", mCursor: " + mCursor + ", counts: " + mCursor.getCount());
+                    //for (int i=0; i<mCursor.getCount(); i++)
+                    //{
+                    //    Log.d(TAG, "mCursor[" + i + "]: " + mCursor.getString(i));
+                    //}
                     mCursor.moveToFirst();
+
+                    int mImageIndex = mCursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                    //mCursor.moveToFirst();
                     String mPath = mCursor.getString(mImageIndex);
-                    File mFile = new File(mPath);
-                    int degree = getImageDegree(mPath);
-                    Log.d(TAG, "mPath: " + mPath + ", degree: " + degree);
+                    mCursor.close();
+                    //Log.d(TAG, "mImageIndex: " + mImageIndex + ", mPath: " + mPath);
+
+                    if ((mPath != null) && (!mPath.equalsIgnoreCase("")))
+                    {
+                        File mFile = new File(mPath);
+                        degree = getImageDegree(mPath);
+                        Log.d(TAG, "degree: " + degree);
+                    }
+                    else
+                    {
+                        mPath = getImageFilePath(uri);
+                        degree = getPictureDegree(uri);
+                        if (degree <= 0)
+                        {
+                            degree = getImageDegree(mPath);
+                            Log.d(TAG, "2 degree: " + degree);
+                        }
+                    }
+                    Log.d(TAG, "mImageIndex: " + mImageIndex + ", mPath: " + mPath);
 
                     try
                     {
-                        Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                        //Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                        Bitmap bitmap = BitmapFactory.decodeStream(this.getContentResolver().openInputStream(uri));
                         if (bitmap.getWidth() > bitmap.getHeight())
-                            ScalePic(bitmap, mPhone.heightPixels, degree);
+                        {
+                            bitmap = ScalePic(bitmap, mPhone.heightPixels, degree);
+                        }
+
+                        mImg.setImageBitmap(bitmap);
                     }
                     catch (FileNotFoundException e)
                     {
@@ -130,7 +161,31 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    private void ScalePic(Bitmap bitmap, int phone, int degree)
+    private Bitmap ScalePic(Bitmap bitmap, int phone, int degree)
+    {
+        float mScale = 1;
+
+        if (bitmap.getWidth() > phone)
+        {
+            mScale = (float) phone / (float)bitmap.getWidth();
+
+            Matrix mMat = new Matrix();
+            mMat.setScale(mScale, mScale);
+            mMat.postRotate((float) degree);
+
+            Bitmap mScaleBitmap = Bitmap.createBitmap(bitmap, 0, 0,
+                    bitmap.getWidth(), bitmap.getHeight(), mMat, false);
+            return mScaleBitmap;
+            //mImg.setImageBitmap(mScaleBitmap);
+        }
+        else
+        {
+            //mImg.setImageBitmap(bitmap);
+            return bitmap;
+        }
+    }
+
+    private void ScalePicOld(Bitmap bitmap, int phone, int degree)
     {
         float mScale = 1;
 
@@ -149,6 +204,51 @@ public class MainActivity extends AppCompatActivity
         else
             mImg.setImageBitmap(bitmap);
     }
+
+    private String getImageFilePath(Uri uri)
+    {
+        // Will return "image:x*"
+        String wholeID = DocumentsContract.getDocumentId(uri);
+
+        // Split at colon, use second item in the array
+        String id = wholeID.split(":")[1];
+
+        String[] column = { MediaStore.Images.Media.DATA };
+
+        // where id is equal to
+        String sel = MediaStore.Images.Media._ID + "=?";
+
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        column, sel, new String[]{ id }, null);
+
+        String filePath = "";
+
+        int columnIndex = cursor.getColumnIndex(column[0]);
+
+        if (cursor.moveToFirst())
+        {
+            filePath = cursor.getString(columnIndex);
+        }
+
+        cursor.close();
+
+        return filePath;
+    }
+
+    private int getPictureDegree(Uri uri)
+    {
+        String[] orientationColumn = {MediaStore.Images.Media.ORIENTATION};
+        Cursor cursor = getContentResolver().query(uri, orientationColumn, null, null, null);
+        int orientation = -1;
+        if (cursor != null && cursor.moveToFirst())
+        {
+            orientation = cursor.getInt(cursor.getColumnIndex(orientationColumn[0]));
+            Log.d(TAG, "Image Orientation: " + orientation);
+        }
+
+        return orientation;
+    }
+
 
     private int getImageDegree(String photoPath)
     {
